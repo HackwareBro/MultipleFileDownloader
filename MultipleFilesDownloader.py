a74301deed2a0b984da033aae718ea0879893b0b
Hackware-Bro <85396426+Hackware-Bro@users.noreply.github.com>
2021-06-09 13:42:32 +0500

    Update MultipleFilesDownloader.py

--- MultipleFilesDownloader.py
+++ MultipleFilesDownloader.py
@@ -4,16 +4,16 @@ dir_path = os.path.join(os.getcwd(),'CN All Lectures')
 if not os.path.exists(dir_path):
     os.mkdir(dir_path,0o666)
 
-for lecNum in range(36):
+for lecNum in range(1,37):
 
-    url = "https://drqasimali.com/wp-content/uploads/2019/01/CN-Lec-"+('0' if lecNum < 9 else '') +str(lecNum+1)+".ppt"
+    url = "https://drqasimali.com/wp-content/uploads/2019/01/CN-Lec-"+('0' if lecNum < 9 else '') +str(lecNum)+".ppt"
 
-    print(f'Downloading Lecture Number {lecNum + 1}')
+    print(f'Downloading Lecture Number {lecNum}')
     response = requests.get(url,stream = True)
     
     if response.status_code == 200:
   
-        with open(os.path.join(dir_path,f'Lecture-{lecNum+1}.ppt'), 'wb') as ppt: 
+        with open(os.path.join(dir_path,f'Lecture-{lecNum}.ppt'), 'wb') as ppt: 
             for block in response.iter_content(chunk_size=1024):
                 ppt.write(block)
 
